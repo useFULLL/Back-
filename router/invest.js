0c4195ffd781560a98fc5f6cd9cc156cd6d70828
sujeong-choi <shalalastal@naver.com>
2020-11-15 17:37:39 +0900

    add router js file

---
+++ router/invest.js
@@ -0,0 +1,29 @@
+var express = require('express'); 
+var router = express.Router();
+var db_config = require('../config/database');
+var conn = db_config.init();
+
+db_config.connect(conn);
+ 
+router.get('/', function(req, res, next) {
+    var sql='SELECT * FROM portfolio;'
+    // 로그인 안 한 경우 
+    //res.render('main',{isLogin:0});
+    // 로그인한 경우
+    conn.query(sql,function(err,results,field){
+        if(err){
+            console.log('err2222');
+        }
+        else{
+            console.log(results);
+            var portfolio = results;
+            res.render('invest',{portfolio});
+
+                    
+        }
+    });
+});
+
+
+
+module.exports = router;
