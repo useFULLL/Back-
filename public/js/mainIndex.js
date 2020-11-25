f639d42f0b2596c5f1dcd9ca19fbd2f80b3cdb8a
pkoi5088 <pkoi5088@gmail.com>
2020-11-25 23:16:30 +0900

    update 완료

--- public/js/mainIndex.js
+++ public/js/mainIndex.js
@@ -2,44 +2,48 @@ var socket = io();
 
 socket.on('send',function(data){
     if(data){
-        var tr_length = $('#topStock tr').length-1;
+        var tr_length = $('#top tr').length-1;
         for(i=1;i<=tr_length;i++){
             for(j=0;j<5;j++){
-                $("#topStock tr:eq("+i+") td:eq("+j+")").html(data[j][i-1]);
+                $("#top tr:eq("+i+") td:eq("+j+")").html(data[j][i-1]);
             }
         }
     }
+    console.log(data);
 });
 
 socket.on('sendFund',function(data){
     if(data){
-        var tr_length = $('#topStock tr').length-1;
+        var tr_length = $('#fund tr').length-1;
         for(i=1;i<=tr_length;i++){
             for(j=0;j<5;j++){
-                $("#topStock tr:eq("+i+") td:eq("+j+")").html(data[j][i-1]);
+                $("#fund tr:eq("+i+") td:eq("+j+")").html(data[j][i-1]);
             }
         }
     }
+    console.log(data);
 });
 
 socket.on('sendTop',function(data){
     if(data){
-        var tr_length = $('#topStock tr').length-1;
+        var tr_length = $('#topstock tr').length-1;
         for(i=1;i<=tr_length;i++){
-            for(j=0;j<5;j++){
-                $("#topStock tr:eq("+i+") td:eq("+j+")").html(data[j][i-1]);
+            for(j=0;j<7;j++){
+                $("#topstock tr:eq("+i+") td:eq("+j+")").html(data[j][i-1]);
             }
         }
     }
+    console.log(data);
 });
 
 socket.on('sendOver',function(data){
     if(data){
-        var tr_length = $('#topStock tr').length-1;
+        var tr_length = $('#overstock tr').length-1;
         for(i=1;i<=tr_length;i++){
             for(j=0;j<5;j++){
-                $("#topStock tr:eq("+i+") td:eq("+j+")").html(data[j][i-1]);
+                $("#overstock tr:eq("+i+") td:eq("+j+")").html(data[j][i-1]);
             }
         }
     }
+    console.log(data);
 });
